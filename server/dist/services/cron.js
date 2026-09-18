4e268fca7fcf3a0445be59acf4bd83e9f27b094b
S Sindid Ahmed Nabil <nabilpwqz@gmail.com>
2026-09-18 22:02:03 +0600

    Update server

--- server/dist/services/cron.js
+++
@@ -1,9 +0,0 @@
-import cron from 'node-cron';
-import { scanInactiveApplications } from './reminder.service';
-export const initCronJobs = () => {
-    cron.schedule('0 9 * * *', async () => {
-        console.log('⏰ Running daily inactivity scanner cron job...');
-        await scanInactiveApplications();
-    });
-    console.log('⚡ Background Cron Job Daemon Active (09:00 AM scanner)');
-};
